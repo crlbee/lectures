8dcde2f6367e5d6357d0ddea8e42520ef4853826
Alexander Mokhov <amokhov@naumen.ru>
2018-10-21 17:22:53 +0500

    added launcher

---
+++ tooling/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+APP_HOME='.'
+java -Xmx256m -Xms50m \
+	-XX:+UseG1GC -XX:G1HeapRegionSize=1 \
+	-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="$APP_HOME/dumps"\
+	-Xloggc:"$APP_HOME/logs/`date +%F_%H-%M-%S`-gc.log" -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCCause\
+	-jar "$APP_HOME/target/tooling-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
