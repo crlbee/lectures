e3cb575100da71f4b46e21eb911506b41511c2ef
Alexander Mokhov <amokhov@naumen.ru>
2018-11-23 22:49:09 +0500

    Xmx 256 -> 128

--- tooling/start.sh
+++ tooling/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 APP_HOME='.'
-java -Xmx256m -Xms50m \
+java -Xmx128m -Xms50m \
 	-XX:+UseG1GC -XX:G1HeapRegionSize=1 \
 	-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="$APP_HOME/dumps"\
 	-Xloggc:"$APP_HOME/logs/`date +%F_%H-%M-%S`-gc.log" -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCCause\
